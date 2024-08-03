78de42f7bda49ce7db6684e26d6fe522239bd79f
Amr Mosallem <amromsllam@gmail.com>
2024-08-03 11:21:24 +0300

    V2.2 Mobile Responsive Design

---
+++ Script/Mobile.js
@@ -0,0 +1,31 @@
+let leftSection = document.getElementById("left-section");
+let mainSection = document.getElementById("main-section");
+
+document.getElementById("show-main-section-button").onclick = function () {
+  mainSection.style.display = "block";
+  leftSection.style.display = "none";
+
+
+};
+
+document.getElementById("show-left-section-button").onclick = function () {
+  mainSection.style.display = "none";
+  leftSection.style.display = "grid";
+};
+function myFunction(x) {
+  if (x.matches) {
+    // If media query matches
+    mainSection.style.display = "block";
+    leftSection.style.display = "grid";
+    leftSection.style.gridTemplateColumns = "75px 1fr";
+  } else {
+    mainSection.style.display = "none";
+  }
+}
+
+// Create a MediaQueryList object
+var x = window.matchMedia("(min-width: 500px)");
+
+x.addEventListener("change", function () {
+  myFunction(x);
+});
